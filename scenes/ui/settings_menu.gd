9281b2238362baff9fa5ba8bf961dd81cff2efa1
Mibeador <deaththe1000th@gmail.com>
2026-07-07 12:04:19 -0700

    Level Select + Settings Menu

--- scenes/ui/settings_menu.gd
+++ scenes/ui/settings_menu.gd
@@ -1,6 +1,6 @@
 extends Control
 
-
+@onready var main = $"../../"
 
 func _on_volume_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(0, value) 
@@ -8,3 +8,7 @@ func _on_volume_value_changed(value: float) -> void:
 
 func _on_mute_toggled(toggled_on: bool) -> void:
 	AudioServer.set_bus_mute(0, toggled_on)
+
+
+func _on_button_pressed() -> void:
+	hide()
--- scripts/controls_menu.gd
+++ scripts/controls_menu.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-@onready var main = $"../../"
+@onready var main = $"../"
 
 
 func _on_resume_pressed() -> void:
