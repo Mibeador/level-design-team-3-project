30b3e357247457a0394115382fbbb5e8939a4c4e
Mibeador <deaththe1000th@gmail.com>
2026-07-07 12:15:32 -0700

    Level Select Mnenu

--- scripts/level_select_menu.gd
+++ scripts/level_select_menu.gd
@@ -1,5 +1,11 @@
 extends Control
 
+@export var level0: PackedScene
+
 
 func _on_exit_pressed() -> void:
 	hide()
+
+
+func _on_level_0_pressed() -> void:
+	get_tree().change_scene_to_packed(level0)
