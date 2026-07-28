8cce70ed05268d04dba006ebeee00d42adf3f1d8
Mibeador <deaththe1000th@gmail.com>
2026-07-28 10:42:51 -0700

    opening scene added

--- scenes/animated_scenes/opening_scene_transition.gd
+++ scenes/animated_scenes/opening_scene_transition.gd
@@ -1,1 +1,23 @@
 extends Node2D
+
+@onready var open_anim: AnimationPlayer = $OpenAnim
+@onready var text1: TypeWriterLabel = $CanvasLayer/StoryText1
+@onready var heartbeat: AudioStreamPlayer = $Heartbeat
+@onready var anim2: AnimationPlayer = $CanvasLayer/StoryText1/AnimationPlayer
+@onready var anim1: AnimationPlayer = $AnimatedSprite2D/LanternLight/AnimationPlayer
+
+func _ready() -> void:
+	heartbeat.pitch_scale = 0.8
+	heartbeat.play()
+	open_anim.play("opening_scene_intro")
+	open_anim.play("Scene Transition")
+	await get_tree().create_timer(11).timeout
+	text1.typewrite("I would do anything...")
+	await get_tree().create_timer(5).timeout
+	text1.typewrite("To see her again...")
+	await get_tree().create_timer(5).timeout
+	anim1.play("light_fade")
+	await get_tree().create_timer(3).timeout
+	anim2.play("text_fade")
+	await get_tree().create_timer(2).timeout
+	get_tree().change_scene_to_file("res://scenes/levels/playtest_2/tutorial_level.tscn")
--- scripts/temp_title.gd
+++ scripts/temp_title.gd
@@ -4,7 +4,7 @@ extends CenterContainer
 @onready var ls_menu = $"../LevelSelectMenu"
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/levels/playtest_2/tutorial_level.tscn")
+	get_tree().change_scene_to_file("res://scenes/animated_scenes/opening_scene_transition.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
